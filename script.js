34970f8e50e27682efed0885d81fd57cd0f78d20
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-09 10:31:40 +0600

    another event explore

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 console.log("hello event");
 let load = ()=>{
-    let head = document.getElementById("head");
-    head.style.color = 'red'
+    // let head = document.getElementById("head");
+    // head.style.color = 'red'
     // alert("loading..........")
+    document.body.style.backgroundColor = 'black'
 }
+// btn click korle funk run hobe
+let showMessage = ()=>{
+    
+    alert("button clicked!")
+}
+// Arrow funk btn clicked code hre now;
+let arrowBtn = document.getElementById("arrowBtn");
+arrowBtn.addEventListener("click",()=>{
+    console.log("arrow function clicked!");
+})
